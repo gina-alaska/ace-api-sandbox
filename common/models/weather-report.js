81ff4c0862df8b89527990e4f4e515818019fe89
Craig Stephenson <cstephen@iarc.uaf.edu>
2016-07-27 11:29:00 -0800

    Add a new weather report endpoint that includes positions.

--- common/models/weather-report.js
+++ common/models/weather-report.js
@@ -1,5 +1,21 @@
 module.exports = function(Weatherreport) {
 	Weatherreport.handleChangeError = function(err) {
-		console.warn("Cannot update change records for WeatherReport: ", err);	
+		console.warn("Cannot update change records for WeatherReport: ", err);
 	};
+
+	Weatherreport.withPositions = function(cb) {
+		Weatherreport.find({
+			include: {
+				relation: 'Position'
+			}
+		}, function(err, results) {
+			if (err) return cb(err);
+			cb(null, results);
+		});
+	};
+
+	Weatherreport.remoteMethod('withPositions', {
+		http: {path: '/with-positions', verb: 'get'},
+		returns: {arg: 'WeatherReports', type: 'string'}
+	});
 };
