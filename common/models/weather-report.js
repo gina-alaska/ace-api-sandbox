a36740be898eca1bc97196881c522143351cd867
Craig Stephenson <crstephenson@alaska.edu>
2016-08-22 13:53:08 -0800

    Add User ID and Date filters to with-position Weather Reports endpoint.

--- common/models/weather-report.js
+++ common/models/weather-report.js
@@ -3,19 +3,36 @@ module.exports = function(Weatherreport) {
 		console.warn("Cannot update change records for WeatherReport: ", err);
 	};
 
-	Weatherreport.withPositions = function(cb) {
+	Weatherreport.withPositions = function(userId, date, cb) {
 		Weatherreport.find({
 			include: {
-				relation: 'Position'
+				relation: 'Position',
+				scope: {
+					where: {
+						timestamp: {
+							gte: new Date(date)
+						}
+					}
+				}
+			},
+			where: {
+				userId: userId
 			}
 		}, function(err, results) {
 			if (err) return cb(err);
-			cb(null, results);
+			var filteredResults = results.filter(function (result) {
+				return result.Position();
+			});
+			cb(null, filteredResults);
 		});
 	};
 
 	Weatherreport.remoteMethod('withPositions', {
 		http: {path: '/with-positions', verb: 'get'},
+		accepts: [
+			{arg: 'userId', type: 'string', 'http': {source: 'query'}, required: true},
+			{arg: 'date', type: 'string', 'http': {source: 'query'}, required: true}
+		],
 		returns: {arg: 'WeatherReports', type: 'string'}
 	});
 };
