fd23938c799b85301d1ec002e18d5ba105b3f894
Craig Stephenson <crstephenson@alaska.edu>
2016-09-09 14:26:21 -0800

    Fix and clean up db_setup.js script.

--- server/boot/db_setup.js
+++ server/boot/db_setup.js
@@ -4,40 +4,38 @@ module.exports = function(app) {
 
 	app.models.mobile_user.find({where: {'username': 'admin'}}, function(err, users) {
 		if(users.length === 0) {
-			app.models.group.create([{name: 'TestUsers'}], function(err, group) {
-				app.models.mobile_user.create([{username: 'testuser', password: 'password', email: 'test@test.com', groupId: group[0].id, apikey: 'none'}], function(err, user) {
-					console.log('Created user:', user[0].username);
-					app.models.position.create([{
+			app.models.mobile_user.create([{username: 'admin', password: 'password', email: 'test@test.com', apikey: 'none'}], function(err, user) {
+				console.log('Created user:', user[0].username);
+				app.models.position.create([{
+					userId: user[0].id,
+					timestamp: '2016-06-08T20:19:04.151Z',
+					'latlng': {
+						'lat': 64.8592668,
+						'lng': -147.8511062
+					}
+				}], function(err, position) {
+					app.models.weatherreport.create([{
 						userId: user[0].id,
-						timestamp: '2016-06-08T20:19:04.151Z',
-						'latlng': {
-							'lat': 64.8592668,
-							'lng': -147.8511062
-						}
-					}], function(err, position) {
-						app.models.weatherreport.create([{
-							userId: user[0].id,
-							positionId: position[0].id,
-							cloudCover: '1/8th',
-							precipitation: 'Light Rain',
-							visibility: 'Mist',
-							temperatureValue: '59',
-							temperatureUnits: ' ºF '
-						}]);
-					});
+						positionId: position[0].id,
+						cloudCover: '1/8th',
+						precipitation: 'Light Rain',
+						visibility: 'Mist',
+						temperatureValue: '59',
+						temperatureUnits: ' ºF '
+					}]);
+				});
 
-					Role.create({
-						name: 'admin'
-					}, function(err, role) {
+				Role.create({
+					name: 'admin'
+				}, function(err, role) {
+					if (err) throw err;
+					console.log('Created role:', role);
+					role.principals.create({
+						principalType: RoleMapping.USER,
+						principalId: user[0].id
+					}, function(err, principal) {
 						if (err) throw err;
-						console.log('Created role:', role);
-						role.principals.create({
-							principalType: RoleMapping.USER,
-							principalId: user[0].id
-						}, function(err, principal) {
-							if (err) throw err;
-							console.log('Created principal:', principal);
-						});
+						console.log('Created principal:', principal);
 					});
 				});
 			});
