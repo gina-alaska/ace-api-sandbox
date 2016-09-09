da8be28904441e77fe87a798911f5165c8f7c7c0
Craig Stephenson <crstephenson@alaska.edu>
2016-09-09 14:23:06 -0800

    Check to see if admin user already exists before creating it.

--- server/boot/db_setup.js
+++ server/boot/db_setup.js
@@ -2,41 +2,45 @@ module.exports = function(app) {
 	var Role = app.models.Role;
 	var RoleMapping = app.models.RoleMapping;
 
-	app.models.group.create([{name: 'TestUsers'}], function(err, group) {
-		app.models.mobile_user.create([{username: 'testuser', password: 'password', email: 'test@test.com', groupId: group[0].id, apikey: 'none'}], function(err, user) {
-			console.log('Created user:', user[0].username);
-			app.models.position.create([{
-				userId: user[0].id,
-				timestamp: '2016-06-08T20:19:04.151Z',
-				'latlng': {
-					'lat': 64.8592668,
-					'lng': -147.8511062
-				}
-			}], function(err, position) {
-				app.models.weatherreport.create([{
-					userId: user[0].id,
-					positionId: position[0].id,
-					cloudCover: '1/8th',
-					precipitation: 'Light Rain',
-					visibility: 'Mist',
-					temperatureValue: '59',
-					temperatureUnits: ' ºF '
-				}]);
-			});
+	app.models.mobile_user.find({where: {'username': 'admin'}}, function(err, users) {
+		if(users.length === 0) {
+			app.models.group.create([{name: 'TestUsers'}], function(err, group) {
+				app.models.mobile_user.create([{username: 'testuser', password: 'password', email: 'test@test.com', groupId: group[0].id, apikey: 'none'}], function(err, user) {
+					console.log('Created user:', user[0].username);
+					app.models.position.create([{
+						userId: user[0].id,
+						timestamp: '2016-06-08T20:19:04.151Z',
+						'latlng': {
+							'lat': 64.8592668,
+							'lng': -147.8511062
+						}
+					}], function(err, position) {
+						app.models.weatherreport.create([{
+							userId: user[0].id,
+							positionId: position[0].id,
+							cloudCover: '1/8th',
+							precipitation: 'Light Rain',
+							visibility: 'Mist',
+							temperatureValue: '59',
+							temperatureUnits: ' ºF '
+						}]);
+					});
 
-			Role.create({
-				name: 'admin'
-			}, function(err, role) {
-				if (err) throw err;
-				console.log('Created role:', role);
-				role.principals.create({
-					principalType: RoleMapping.USER,
-					principalId: user[0].id
-				}, function(err, principal) {
-					if (err) throw err;
-					console.log('Created principal:', principal);
+					Role.create({
+						name: 'admin'
+					}, function(err, role) {
+						if (err) throw err;
+						console.log('Created role:', role);
+						role.principals.create({
+							principalType: RoleMapping.USER,
+							principalId: user[0].id
+						}, function(err, principal) {
+							if (err) throw err;
+							console.log('Created principal:', principal);
+						});
+					});
 				});
 			});
-		});
+		}
 	});
 };
